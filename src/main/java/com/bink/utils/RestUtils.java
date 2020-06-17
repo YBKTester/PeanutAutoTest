44c344c325fe33711fb936662bde0f92cb973efd
yangbk29426 <515930316@qq.com>
2020-06-17 09:16:57 +0800

    第一次提交

---
+++ src/main/java/com/bink/utils/RestUtils.java
@@ -0,0 +1,40 @@
+package com.bink.utils;
+
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * 工具类
+ *
+ * @author Bink
+ * @date 2020/6/16
+ */
+public class RestUtils {
+
+    /**
+     * Post打印调试方法
+     *
+     * @param url       请求地址
+     * @param cookie    cookie
+     * @param paramsMap 请求参数
+     */
+    public static void postPrint(String url, String cookie, Map<String, Object> paramsMap) {
+        Response response = given().cookie(cookie).log().all().params(paramsMap).post(url);
+        response.prettyPrint();
+    }
+
+    /**
+     * Get打印调试方法
+     *
+     * @param url       请求地址
+     * @param cookie    cookie
+     * @param paramsMap 请求参数
+     */
+    public static void getPrint(String url, String cookie, Map<String, Object> paramsMap) {
+        Response response = given().cookie(cookie).log().all().params(paramsMap).get(url);
+        response.prettyPrint();
+    }
+}
