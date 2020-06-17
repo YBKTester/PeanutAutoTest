6a7d2927edba65b425982490b11a6433328fa8d2
unknown <yangbk29426@hs.handsome.com.cn>
2020-06-17 09:13:05 +0800

    增加请求地址常量类

---
+++ src/main/java/com/bink/common/UrlCommon.java
@@ -0,0 +1,12 @@
+package com.bink.common;
+
+/**
+ * 常用请求地址
+ *
+ * @author Bink
+ * @date 2020/6/16
+ */
+public class UrlCommon {
+    public static final String BAIDU_URL = "https://www.baidu.com";
+    public static final String TEST_URL = "http://jsonplaceholder.typicode.com/";
+}
