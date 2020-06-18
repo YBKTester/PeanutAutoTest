db943001b061ced558304c475de40c9b69508cb2
yangbk29426 <515930316@qq.com>
2020-06-18 14:42:15 +0800

    接口地址常量

---
+++ src/main/java/com/bink/common/ApiPathCommon.java
@@ -0,0 +1,14 @@
+package com.bink.common;
+
+/**
+ * 接口地址常量
+ *
+ * @author Bink
+ * @date 2020/6/18
+ */
+public class ApiPathCommon {
+    /**
+     * 获取当前用户信息接口地址
+     */
+    public static final String GET_CURR_USER_INFO = "/getCurrUserInfo";
+}
