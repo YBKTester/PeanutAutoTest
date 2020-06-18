af4820d17c2b53794a2069b2cd1a71935eade3c4
yangbk29426 <515930316@qq.com>
2020-06-18 14:44:40 +0800

    新增打印方法，增加请求方式JSON/Urlencoded

--- src/main/java/com/bink/utils/RestUtils.java
+++ src/main/java/com/bink/utils/RestUtils.java
@@ -22,9 +22,36 @@ public class RestUtils {
      * @param cookie    cookie
      * @param paramsMap 请求参数
      */
-    public static void postPrint(String url, String cookie, Map<String, Object> paramsMap) {
+    public static void postPrintByJson(String url, String cookie, Map<String, Object> paramsMap) {
         Response response = given().
                 contentType(ContentType.JSON).
+                cookie(cookie).
+                log().all().
+                params(paramsMap).
+                post(url);
+        response.prettyPrint();
+    }
+
+    public static void postPrintByJson(String url, String cookieName, String cookieValue, Map<String, Object> paramsMap) {
+        Response response = given().
+                contentType(ContentType.JSON).
+                cookie(cookieName, cookieValue).
+                log().all().
+                params(paramsMap).
+                post(url);
+        response.prettyPrint();
+    }
+
+    /**
+     * Post打印调试方法
+     *
+     * @param url       请求地址
+     * @param cookie    cookie
+     * @param paramsMap 请求参数
+     */
+    public static void postPrintByURLENC(String url, String cookie, Map<String, Object> paramsMap) {
+        Response response = given().
+                contentType(ContentType.URLENC).
                 cookie(cookie).
                 log().all().
                 params(paramsMap).
@@ -42,5 +69,46 @@ public class RestUtils {
     public static void getPrint(String url, String cookie, Map<String, Object> paramsMap) {
         Response response = given().contentType(ContentType.JSON).cookie(cookie).log().all().params(paramsMap).get(url);
         response.prettyPrint();
+    }
+
+    /**
+     * 获取响应体返回值
+     *
+     * @param url       请求地址
+     * @param cookie    cookie
+     * @param paramsMap 请求参数
+     * @param jsonPath  目标路径JsonPath表达式
+     * @return 返回所需值
+     */
+    public static String getValueByPath(String url, String cookie, Map<String, Object> paramsMap, String jsonPath) {
+        String value = given().
+                cookie(cookie).
+                params(paramsMap).log().all().
+                post(url).
+                body().
+                path(jsonPath).
+                toString();
+        System.out.println("所需返回值为：" + value);
+        return value;
+    }
+
+    /**
+     * 获取响应体返回值[登陆专用]
+     * 无需Cookie
+     *
+     * @param url       请求地址
+     * @param paramsMap 请求参数
+     * @param jsonPath  目标路径JsonPath表达式
+     * @return 返回所需值
+     */
+    public static String getValueByPath(String url, Map<String, Object> paramsMap, String jsonPath) {
+        String value = given().
+                params(paramsMap).log().all().
+                post(url).
+                body().
+                path(jsonPath).
+                toString();
+        System.out.println("所需返回值为：" + value);
+        return value;
     }
 }
