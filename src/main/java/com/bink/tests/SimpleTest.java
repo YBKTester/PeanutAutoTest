90289d7dfdd1fb0ec2be6ed0a4bf4e574560773f
yangbk29426 <515930316@qq.com>
2020-06-24 14:37:35 +0800

    测试数据库操作

---
+++ src/main/java/com/bink/tests/SimpleTest.java
@@ -0,0 +1,42 @@
+package com.bink.tests;
+
+import com.bink.dao.ParamsMapper;
+import com.bink.dao.UserMapper;
+import com.bink.entity.Params;
+import com.bink.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @author Bink
+ * @date 2020/6/24
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleTest {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private ParamsMapper paramsMapper;
+
+    @Test
+    public void test() {
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void paramsTest() {
+        List<Params> paramsList = paramsMapper.selectList(null);
+        Assert.assertEquals(1, paramsList.size());
+        paramsList.forEach(params -> System.out.println(params.getCaseName()));
+        System.out.println("测试结束！！！");
+    }
+}
