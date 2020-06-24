d11523cd87952f3899e1a8538af12a4a3172843c
yangbk29426 <515930316@qq.com>
2020-06-24 14:37:21 +0800

    实体类对应数据库操作类Mapper

---
+++ src/main/java/com/bink/dao/ParamsMapper.java
@@ -0,0 +1,11 @@
+package com.bink.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bink.entity.Params;
+
+/**
+ * @author Bink
+ * @date 2020/6/24
+ */
+public interface ParamsMapper extends BaseMapper<Params> {
+}
